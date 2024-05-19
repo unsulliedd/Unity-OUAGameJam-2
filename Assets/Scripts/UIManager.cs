dbde98a6a211da3d6eb9bf0a529177e9902d1948
unsulliedd <berkkarakas98@hotmail.com>
2024-05-19 18:59:53 +0300

    Add basic HUD

--- Assets/Scripts/UIManager.cs
+++ Assets/Scripts/UIManager.cs
@@ -1,12 +1,16 @@
-using System.Collections;
-using System.Collections.Generic;
+using TMPro;
 using UnityEngine;
 using UnityEngine.UI;
 
 public class UIManager : MonoBehaviour
 {
     private PlayerBuilding playerBuilding;
-    [SerializeField]private GameObject buildMenuPanel;
+
+    [SerializeField] private GameObject buildMenuPanel;
+    [SerializeField] private TextMeshProUGUI _healthText;
+    [SerializeField] private TextMeshProUGUI _killsText;
+    [SerializeField] private TextMeshProUGUI _ammoText;
+    [SerializeField] private TextMeshProUGUI _timeAbilityText;
 
     void Start()
     {
@@ -27,5 +31,25 @@ public class UIManager : MonoBehaviour
     public void CloseBuildMenu()
     {
         buildMenuPanel.SetActive(false);
+    }
+
+    public void UpdateHealthText(int health)
+    {
+        _healthText.text = health.ToString();
+    }
+
+    public void UpdateKillsText(int kills)
+    {
+        _killsText.text = kills.ToString();
+    }
+
+    public void UpdateAmmoText(int ammo)
+    {
+        _ammoText.text = ammo.ToString();
+    }
+
+    public void UpdateTimeAbilityText(float time)
+    {
+        _timeAbilityText.text = time.ToString();
     }
 }
